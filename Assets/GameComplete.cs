4e1a6ef28ceebbee1c8308e6a84833af092080ca
vanikiopik <vanikiopik@gmail.com>
2023-05-04 20:02:02 +0300

    UPD: made GUI complete level notification + Word Doc

---
+++ Assets/GameComplete.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameComplete : MonoBehaviour
+{
+    [SerializeField] Text _text;
+
+
+    private void Awake()
+    {
+        _text.enabled = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            _text.enabled = true;
+
+    }
+}
